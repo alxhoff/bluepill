0746d3c2b32d1d71f745c007b190e3c6bb22d599
Alex Hoffman <alxhoff@gmail.com>
2020-04-01 12:29:33 +0200

    Gitignore updated

--- build/CMakeFiles/clean_lib.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/clean_lib"
-  "cleanlib"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/clean_lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
